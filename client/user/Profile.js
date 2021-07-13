2c78ab6bb9edc8e3899bdac85d1a5b5214e9432b
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-07-12 20:53:20 -0700

    posts, likes, comments, newsfeed

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import {makeStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction' 
+import ListItemText from '@material-ui/core/ListItemText' 
 import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
@@ -13,11 +13,11 @@ import Edit from '@material-ui/icons/Edit'
 import Divider from '@material-ui/core/Divider'
 import DeleteUser from './DeleteUser'
 import auth from './../auth/auth-helper'
-import ProfileTabs from './ProfileTabs'
-import FollowProfileButton from './FollowProfileButton'
 import {read} from './api-user.js'
 import {Redirect, Link} from 'react-router-dom'
-
+import FollowProfileButton from './../user/FollowProfileButton'
+import ProfileTabs from './../user/ProfileTabs'
+import {listByUser} from './../post/api-post.js'
 
 const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
@@ -27,8 +27,9 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(5)
   }),
   title: {
-    marginTop: theme.spacing(3),
-    color: theme.palette.protectedTitle
+    margin: `${theme.spacing(2)}px ${theme.spacing(1)}px 0`,
+    color: theme.palette.protectedTitle,
+    fontSize: '1em'
   },
   bigAvatar: {
     width: 60,
@@ -44,12 +45,13 @@ export default function Profile({ match }) {
     redirectToSignin: false,
     following: false
   })
+  const [posts, setPosts] = useState([])
   const jwt = auth.isAuthenticated()
 
   useEffect(() => {
     const abortController = new AbortController()
     const signal = abortController.signal
-
+  
     read({
       userId: match.params.userId
     }, {t: jwt.token}, signal).then((data) => {
@@ -57,10 +59,10 @@ export default function Profile({ match }) {
         setValues({...values, redirectToSignin: true})
       } else {
         let following = checkFollow(data)
-        setValues({...values, user: data, following: following})  
+        setValues({...values, user: data, following: following})
+        loadPosts(data._id)
       }
     })
-
     return function cleanup(){
       abortController.abort()
     }
@@ -86,11 +88,29 @@ export default function Profile({ match }) {
       }
     })
   }
+  const loadPosts = (user) => {
+    listByUser({
+      userId: user
+    }, {
+      t: jwt.token
+    }).then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        setPosts(data)
+      }
+    })
+  }
+  const removePost = (post) => {
+    const updatedPosts = posts
+    const index = updatedPosts.indexOf(post)
+    updatedPosts.splice(index, 1)
+    setPosts(updatedPosts)
+  }
 
-  const photoUrl = values.user._id
+    const photoUrl = values.user._id
               ? `/api/users/photo/${values.user._id}?${new Date().getTime()}`
               : '/api/users/defaultphoto'
-
     if (values.redirectToSignin) {
       return <Redirect to='/signin'/>
     }
@@ -104,27 +124,28 @@ export default function Profile({ match }) {
             <ListItemAvatar>
               <Avatar src={photoUrl} className={classes.bigAvatar}/>
             </ListItemAvatar>
-            <ListItemText primary={values.user.name} secondary={values.user.email}/> 
-            {auth.isAuthenticated().user && auth.isAuthenticated().user._id == values.user._id ?
-              (<ListItemSecondaryAction>
-                <Link to={"/user/edit/" + values.user._id}>
-                  <IconButton aria-label="Edit" color="primary">
-                    <Edit/>
-                  </IconButton>
-                </Link>
-                <DeleteUser userId={values.user._id}/>
-              </ListItemSecondaryAction>) :
-              (<FollowProfileButton following={values.following} onButtonClick={clickFollowButton} />)
+            <ListItemText primary={values.user.name} secondary={values.user.email}/> {
+             auth.isAuthenticated().user && auth.isAuthenticated().user._id == values.user._id
+             ? (<ListItemSecondaryAction>
+                  <Link to={"/user/edit/" + values.user._id}>
+                    <IconButton aria-label="Edit" color="primary">
+                      <Edit/>
+                    </IconButton>
+                  </Link>
+                  <DeleteUser userId={values.user._id}/>
+                </ListItemSecondaryAction>)
+            : (<FollowProfileButton following={values.following} onButtonClick={clickFollowButton}/>)
             }
           </ListItem>
           <Divider/>
           <ListItem>
-            <ListItemText primary={"Joined: " + (
+            <ListItemText primary={values.user.about} secondary={"Joined: " + (
               new Date(values.user.created)).toDateString()}/>
-              <ListItemText primary={values.user.about}/>
           </ListItem>
         </List>
-        <ProfileTabs user={values.user} />
+        <ProfileTabs user={values.user} posts={posts} removePostUpdate={removePost}/>
       </Paper>
     )
-  }
+}
+
+
--- client/user/ProfileTabs.js
+++ client/user/ProfileTabs.js
@@ -5,6 +5,7 @@ import Typography from '@material-ui/core/Typography'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import FollowGrid from './../user/FollowGrid'
+import PostList from './../post/PostList'
 
 export default function ProfileTabs ( props ){
   const [tab, setTab] = useState(0)
@@ -28,7 +29,7 @@ export default function ProfileTabs ( props ){
             <Tab label="Followers" />
           </Tabs>
         </AppBar>
-       {tab === 0 && <TabContainer></TabContainer>}
+       {tab === 0 && <TabContainer><PostList removeUpdate={props.removePostUpdate} posts={props.posts}/></TabContainer>}
        {tab === 1 && <TabContainer><FollowGrid people={props.user.following}/></TabContainer>}
        {tab === 2 && <TabContainer><FollowGrid people={props.user.followers}/></TabContainer>}
     </div>)
